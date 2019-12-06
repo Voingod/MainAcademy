6d6f1209fb177f99ea03c90632eefc73b406135b
Voingod <voingodin@gmail.com>
2019-12-07 00:05:25 +0200

    #TDP-1. Угадывание числа Задается случайное чсило. В цикле делаем ввод из клавиатуры до тех пор, пока не  угадаем число. После пятого неверного раза выводится подсказка

--- Lab 1.3.1/Hello_Operators_advstud/Program.cs
+++ Lab 1.3.1/Hello_Operators_advstud/Program.cs
@@ -14,8 +14,31 @@ namespace Hello_Operators_advstud
 
             Random random = new Random();
             // random.Next(MaxValue) returns a 32-bit signed integer that is greater than or equal to 0 and less than MaxValue
-            int Guess_number = random.Next(MyMax) + 1;
+            int guessNumber = random.Next(MyMax) + 1;
             // implement input of number and comparison result message in the while circle with  comparison condition
+            int inputNumber;
+            byte count=0;
+            Console.Write("Enter a number between 0 and 200: ");
+            int.TryParse(Console.ReadLine(), out inputNumber);
+            while (inputNumber != guessNumber)
+            {
+                count++;
+                Console.Write("You don't guess, please enter a number again: ");
+                int.TryParse(Console.ReadLine(), out inputNumber);
+                if (count > 4)
+                {
+                    string hint = guessNumber < inputNumber ? $"Number less than {inputNumber}" 
+                        : $"Number more than {inputNumber}";
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(hint);
+                    Console.ResetColor();
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Congratulation, the number is {guessNumber}");
+            Console.ReadLine();
+
+
         }
     }
 }
