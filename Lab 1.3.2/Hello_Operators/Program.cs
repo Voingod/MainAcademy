1876e1e9131c8c1d0f0d0db3e1fef37079be8be4
Voingod <voingodin@gmail.com>
2019-12-08 00:44:31 +0200

    # TDP-2. Реализация методов             1. Farmer, wolf, goat and cabbage puzzle             2. Console calculator             3. Factirial calculation

--- Lab 1.3.2/Hello_Operators/Program.cs
+++ Lab 1.3.2/Hello_Operators/Program.cs
@@ -60,7 +60,94 @@ You can do whatever how many flights. How to transport the wolf, goat and cabbag
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Please,  type numbers by step ");
             // Implement input and checking of the 7 numbers in the nested if-else blocks with the  Console.ForegroundColor changing
-
+            
+            int input = int.Parse(Console.ReadLine());
+            if (input == 3) 
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                input = int.Parse(Console.ReadLine());
+                if (input == 8) 
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    int inputVariant= int.Parse(Console.ReadLine());
+                    if (inputVariant == 1 || inputVariant == 2) 
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        input = int.Parse(Console.ReadLine());
+                        if (input == 7) 
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            input = int.Parse(Console.ReadLine());
+                            if (input == 2 && inputVariant != 2) 
+                            {
+                                Console.ForegroundColor = ConsoleColor.Magenta;
+                                input = int.Parse(Console.ReadLine());
+                                if (input == 8)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    input = int.Parse(Console.ReadLine());
+                                    if (input == 3)
+                                    {
+                                        Console.WriteLine("Congratulation, you are winner!!!");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Sorry, but you are lose");
+                                }
+                            }
+                            else if (input == 1 && inputVariant != 1)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                input = int.Parse(Console.ReadLine());
+                                if (input == 8)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    input = int.Parse(Console.ReadLine());
+                                    if (input == 3)
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.Green;
+                                        Console.WriteLine("Congratulation, you are winner!!!");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Sorry, but you are lose");
+                                }
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Sorry, but you are lose");
+                            }
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Sorry, but you are lose");
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Sorry, but you are lose");
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Sorry, but you are lose");
+                }
+                
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sorry, but you are lose");
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
         }
         #endregion
 
@@ -68,6 +155,7 @@ You can do whatever how many flights. How to transport the wolf, goat and cabbag
         static void Calculator()
         {
             // Set Console.ForegroundColor  value
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(@"Select the arithmetic operation:
 1. Multiplication 
 2. Divide 
@@ -77,6 +165,81 @@ You can do whatever how many flights. How to transport the wolf, goat and cabbag
             // Implement option input (1,2,3,4,5)
             //     and input of  two or one numbers
             //  Perform calculations and output the result 
+
+            int number;
+            double result = 0;
+            double number_one = 0;
+            double number_two = 0;
+            
+            if(!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Enter the inneger number");
+                return;
+            }
+            Console.WriteLine();            
+            switch(number)
+            {
+                case 1:
+                    {
+                        if(Enter(ref number_one, ref number_two))
+                            result = number_one * number_two;
+                        break;
+                    }
+                case 2:
+                    {
+                        if (Enter(ref number_one, ref number_two))
+                        {
+                            if (number_two != 0)
+                                result = number_one / number_two;
+                            else
+                            { 
+                                Console.WriteLine("number_two can't be zero");
+                                return;
+                            }
+                        }                    
+                        break;
+                    }
+                case 3:
+                    {
+                        if(Enter(ref number_one, ref number_two))
+                            result = number_one + number_two;
+                        break;
+                    }
+                case 4:
+                    {
+                        if(Enter(ref number_one, ref number_two))
+                            result = number_one - number_two;
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.Write("Enter number: ");
+                        if(!double.TryParse(Console.ReadLine(), out number_one))
+                        {
+                            Console.WriteLine("Incorrect number");
+                            return;
+                        }
+                        result = 1;
+                        for (int i = 0; i < Math.Abs(number_one); i++)
+                        {
+                            result *= Math.E;
+                        }
+                        if (number_one < 0)
+                        {
+                            result = 1 / result;
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Incorrect operation");
+                        break;
+                    }
+            }
+            Console.WriteLine("Result is: "+result);
+
+            
+
         }
         #endregion
 
@@ -86,7 +249,56 @@ You can do whatever how many flights. How to transport the wolf, goat and cabbag
             // Implement input of the number
             // Implement input the for circle to calculate factorial of the number
             // Output the result
+            Console.WriteLine();
+            Console.Write("Enter a number: ");
+            int number;
+            int factorial = 1;
+
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                if (number < 0)
+                {
+                    Console.WriteLine($"Number must be more than 0");
+                }
+                else if (number == 0)
+                {
+                    Console.WriteLine($"{number} factorial is {factorial}");
+                }
+                else
+                {
+                    for (int i = number; i > 0; i--) 
+                    {
+                        factorial *= i;
+                    }
+                    Console.WriteLine($"{number} factorial is {factorial}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"You must enter the integer number");
+            }
+
+
+
+
         }
         #endregion
+
+        static bool Enter(ref double number_one, ref double number_two)
+        {
+            Console.Write("Enter number_one: ");
+            if(!double.TryParse(Console.ReadLine(), out number_one))
+            {
+                Console.WriteLine("Incorrect number");
+                return false;
+            }
+            Console.Write("Enter number_two: ");
+            if(!double.TryParse(Console.ReadLine(), out number_two))
+            {
+                Console.WriteLine("Incorrect number");
+                return false;
+            }
+            return true;
+        }
     }
 }
