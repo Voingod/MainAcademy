b056b4968452fa382e9ae34c9ad78797b38f1bbf
Voingod <voingodin@gmail.com>
2019-12-23 01:04:13 +0200

    #TDP-1. Создание шаблона для лабы
    1.Добавил свойства
    2.Реализовал метод для отрисовки прямоугольника

---
+++ Lab 2.2.2/Hello_Cons_Dr_Methods/Box.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Box
+    {
+        //1.  Implement public  auto-implement properties for start position (point position)
+        //auto-implement properties for width and height of the box
+        //and auto-implement properties for a symbol of a given set of valid characters (*, + ,.) to be used for the border 
+        //and a message inside the box
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public char Symbol { get; set; }
+        public string Message { get; set; }
+        //2.  Implement public Draw() method
+        //to define start position, width and height, symbol, message  according to properties
+        //Use Math.Min() and Math.Max() methods
+        //Use draw() to draw the box with message
+        public void Draw()
+        {
+            draw(X, Y, Width, Height, Symbol, Message);
+        }
+
+        //3.  Implement private method draw() with parameters 
+        //for start position, width and height, symbol, message
+        //Change the message in the method to return the Box square
+        //Use Console.SetCursorPosition() method
+        //Trim the message if necessary
+        private void draw(int x, int y, int width, int height, char symbol, string message)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == height - 1 || j == width - 1|| i == 0 || j == 0) 
+                    {
+                        Console.SetCursorPosition(x + i, y + j);
+                        Console.Write(symbol);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ Lab 2.2.2/Hello_Cons_Dr_Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Implement start position, width and height, symbol, message input
+                //Create Box class instance
+                Box box = new Box();
+                //Use  Box.Draw() method
+                Random random = new Random();
+                do
+                {
+                    Console.Clear();
+                    box.X = random.Next(5, 25);
+                    box.Y = random.Next(5, 25);
+                    box.Width = random.Next(5, 25);
+                    box.Height = random.Next(5, 25);
+                    box.Symbol = '*';
+                    box.Message = "Draw something";
+                    box.Draw();
+                } while (Console.ReadKey().Key != ConsoleKey.Backspace);
+                //Console.WriteLine("Press any key...");
+            
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
