6acbf4756976d75492ce22257e042ce51d89b70d
Voingod <voingodin@gmail.com>
2019-12-22 21:37:08 +0200

    #TDP-1. Рекурсивная реализация факториала

---
+++ Lab 2.2.1/Hello_fact_advstud/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_fact_advstud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define parameters to calculate the factorial of
+            //Call fact() method to calculate
+
+            uint number = 10;
+            Console.WriteLine(Factorial(number));
+            Console.ReadLine();
+        }
+
+        //Create fact() method  with parameter to calculate factorial
+        //Use ternary operator
+        public static uint Factorial(uint number)
+        {
+            return number == 0 ? 1 : Factorial(number - 1) * number;
+        }
+    }
+
+    
+
+}
