ead81f6405eb684a26ab66b57f3bbabcddc8fa39
Voingod <voingodin@gmail.com>
2019-12-22 20:23:49 +0200

    #TDP-1. Выполнение работы согласно ТЗ

--- Lab 2.1/CSharp_Net_module1_2_1_lab/LibraryUser.cs
+++ Lab 2.1/CSharp_Net_module1_2_1_lab/LibraryUser.cs
@@ -5,34 +5,45 @@ namespace CSharp_Net_module1_2_1_lab
 
     class LibraryUser : ILibraryUser
     {
-        
+        private const int bookLimit = 5;
         public string FirstName {get;}
         public string LastName { get; }
         public int Id { get; }
         public string Phone { get; set; }
-        public int BookLimit { get; }
-      
+        static public int BookLimit 
+        {
+            get => bookLimit;
+        }
 
-        private string[] bookList = new string[5];//
+        private string[] bookList = new string[BookLimit];
+
+        private bool LimitCheck(int BookList)
+        {
+            return BookList >= 0 && BookList <= BookLimit - 1;
+        }
         public string this[int BookList]
         {
             get
             {
-                if (BookList >= 0 && BookList <= BookLimit - 1) 
+                if (LimitCheck(BookList))  
                 {
                     return bookList[BookList];
                 }
                 else
                 {
-                    return null;
+                    throw new IndexOutOfRangeException($"Book with index {BookList} not found");
                 }
                 
             }
             set
             {
-                if (BookList >= 0 && BookList <= BookLimit - 1)
+                if (LimitCheck(BookList))
                 {
                     bookList[BookList] = value;
+                }
+                else
+                {
+                     throw new IndexOutOfRangeException($"You can have only {BookLimit} books");
                 }
             }
         }
@@ -48,26 +59,58 @@ namespace CSharp_Net_module1_2_1_lab
             FirstName = "Unknown";
             LastName = "Unknown";
             Phone = "None";
+            Id = -1;
         }
 
         public void AddBook(string addBook)
         {
-            bookList[0] = "11";
+            for (int i = 0; i < bookList.Length; i++)
+            {
+                if (bookList[i] == null)
+                {
+                    bookList[i] = addBook;
+                    break;
+                }
+                else if (i == bookList.Length - 1) 
+                {
+                    Console.WriteLine($"You can have only {BookLimit} books");
+                }
+            }
         }
 
         public void RemoveBook(string removeBook)
         {
-            throw new NotImplementedException();
+
+            for (int i = 0; i < bookList.Length; i++)
+            {
+                if (bookList[i] == removeBook)
+                {
+                    bookList[i] = null;
+                    break;
+                }
+            }
         }
 
         public void BookInfo(int booksIndex)
         {
-            throw new NotImplementedException();
+            if (booksIndex < BookLimit) 
+                Console.WriteLine(bookList[booksIndex]);
+            else
+                Console.WriteLine($"Book with index {booksIndex} not found");
         }
 
         public string BooksCount()
         {
-            throw new NotImplementedException();
+            int currentBooksCount = 0;
+            for (int i = 0; i < bookList.Length; i++)
+            {
+                if (bookList[i] != null)
+                {
+                    currentBooksCount++;
+                }
+            }
+            return currentBooksCount.ToString();
+           // Console.WriteLine($"You have {currentBooksCount} books out of {BookLimit}");
         }
     }
 
--- Lab 2.1/CSharp_Net_module1_2_1_lab/Program.cs
+++ Lab 2.1/CSharp_Net_module1_2_1_lab/Program.cs
@@ -8,28 +8,58 @@ namespace CSharp_Net_module1_2_1_lab
         static void Main(string[] args)
         {
             // 8) declare 2 objects. Use default and paremeter constructors
-            LibraryUser user1 = new LibraryUser(), user2 = new LibraryUser("Maria", "Ivanenko", "+380447777777", 2);
+            LibraryUser user1 = new LibraryUser(), 
+                        user2 = new LibraryUser("Maria", "Ivanenko", "+380447777777", 2);
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("User1 " + user1.FirstName + " " + user1.LastName);
             Console.WriteLine ("User2 " + user2.FirstName + " " + user2.LastName);
-            
+            Console.ResetColor();
+            Console.WriteLine();
+
             // 9) do operations with books for all users: run all methods for both objects
             Console.WriteLine("User 1: add Harry Potter");
             user1.AddBook("Harry Potter");
+            Console.WriteLine();
+
             Console.WriteLine("User 2: add Sherlock Holmes");
             user2.AddBook("Sherlock Holmes");
+            Console.WriteLine();
+
             Console.WriteLine("user1.BooksCount = " + user1.BooksCount() + "; user2.BooksCount " + user2.BooksCount());
-            Console.WriteLine("user2 :");
+            Console.Write("user2:");
             Console.WriteLine("Add Kobzar");
             user2.AddBook("Kobzar");
+            Console.WriteLine();
+
             Console.WriteLine("user2.BooksCount " + user2.BooksCount());
             Console.WriteLine("Add Dorian Gray");
             user2.AddBook("Dorian Gray");
+            Console.WriteLine();
+
+            Console.WriteLine($"List of {user2.FirstName}'s books");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < LibraryUser.BookLimit; i++)
+            {
+                user2.BookInfo(i);
+            }
+            Console.ResetColor();
+
             Console.WriteLine("user2.BooksCount " + user2.BooksCount());
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("user2 books " + user2[0] + "\n" + user2[1]);
+            Console.ResetColor();
             Console.WriteLine("Remove Sherlock Holmes");
             user2.RemoveBook("Sherlock Holmes");
-            Console.WriteLine("user2.BooksCount "+user2.BooksCount());
+            Console.WriteLine();
 
+            Console.WriteLine("user2.BooksCount "+user2.BooksCount());
+            Console.WriteLine($"List of {user2.FirstName}'s books after delete");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < LibraryUser.BookLimit; i++)
+            {
+                user2.BookInfo(i);
+            }
+            Console.ResetColor();
             Console.ReadLine();
 
         }
