0f36a77fa2843611fd5c0cca0793d07110badbc5
Voingod <voingodin@gmail.com>
2019-12-25 22:12:55 +0200

    #TDP-1. Реализация перегрузки операторов 1. Реализовал перегрузку согласно ТЗ

---
+++ Lab 2.3/CSharp_Net_module1_2_3_lab/Money.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_2_3_lab
+{
+    // 1) declare enumeration CurrencyTypes, values UAH, USD, EU
+    enum CurrencyTypes { UAH, USD, EU }
+
+    class Money
+    {
+
+        // 2) declare 2 properties Amount, CurrencyType
+        public double Amount { get; set; }
+        public CurrencyTypes CurrencyType { get; set; }
+        // 3) declare parameter constructor for properties initialization
+        public Money(double amount, CurrencyTypes currencyType)
+        {
+            Amount = amount;
+            CurrencyType = currencyType;
+        }
+        // 4) declare overloading of operator + to add 2 objects of Money
+        public static double operator +(Money m1, Money m2)
+        {
+            return m1.Amount + m2.Amount;
+        }
+        public static double operator +(Money m1, double m2)
+        {
+            return m1.Amount + m2;
+        }
+        // 5) declare overloading of operator -- to decrease object of Money by 1
+        public static Money operator --(Money m1)
+        {
+            Money money = new Money(--m1.Amount, m1.CurrencyType);
+            return money;
+        }
+
+        // 6) declare overloading of operator * to increase object of Money 3 times
+        public static Money operator *(Money m1, int m2)
+        {
+            Money money = new Money(m1.Amount * m2, m1.CurrencyType);
+            return money;
+        }
+        // 7) declare overloading of operator > and < to compare 2 objects of Money
+        public static bool operator <(Money m1, Money m2)
+        {
+            return m1.Amount < m2.Amount;
+        }
+        public static bool operator >(Money m1, Money m2)
+        {
+            return m1.Amount > m2.Amount;
+        }
+        public static bool operator <(Money m1, string m2)
+        {
+            if (!int.TryParse(m2, out int money))
+            {
+                Console.Write("Entered value is uncorrect ");
+                return false;
+            }
+            return m1.Amount < money;
+        }
+        public static bool operator >(Money m1, string m2)
+        {
+            if (!int.TryParse(m2, out int money))
+            {
+                Console.Write("Entered value is uncorrect ");
+                return false;
+            }
+            return m1.Amount > money;
+        }
+        // 8) declare overloading of operator true and false to check CurrencyType of object
+        public static bool operator true(Money m1)
+        {
+            if (m1.CurrencyType == CurrencyTypes.USD || m1.CurrencyType == CurrencyTypes.EU)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator false(Money m1)
+        {
+            if (m1.CurrencyType != CurrencyTypes.USD || m1.CurrencyType != CurrencyTypes.EU)
+            {
+                return true;
+            }
+            return false;
+        }
+        // 9) declare overloading of implicit/ explicit conversion  to convert Money to double, string and vice versa
+        public static explicit operator double(Money m)
+        {
+            return (double)m.Amount;
+        }
+        public static explicit operator Money(double m)
+        {
+            Money money = new Money(m, CurrencyTypes.EU);
+            return money;
+        }
+        public static explicit operator string(Money m)
+        {
+            return m.Amount.ToString()+" "+m.CurrencyType.ToString();
+        }
+        public static explicit operator Money(string m)
+        {
+            Money resultMoney = null;
+            if (!double.TryParse(m, out double money))
+            {
+                Console.WriteLine("Entered value is uncorrect");
+            }
+            else
+            {
+                resultMoney = new Money (money, CurrencyTypes.EU);
+            }
+            return resultMoney;
+        }
+        public override string ToString()
+        {
+            return Amount.ToString() +" "+ CurrencyType.ToString();
+        }
+    }
+}
---
+++ Lab 2.3/CSharp_Net_module1_2_3_lab/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_2_3_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10) declare 2 objects
+            Money dollar = new Money(30, CurrencyTypes.USD);
+            Money euro = new Money(50, CurrencyTypes.EU);
+            // 11) do operations
+            // add 2 objects of Money
+            Console.WriteLine(dollar + euro);
+            // add 1st object of Money and double
+            Console.WriteLine(dollar + 20.15);
+            // decrease 2nd object of Money by 1 
+            Console.WriteLine(euro--);
+            // increase 1st object of Money
+            Console.WriteLine(euro * 3);
+            // compare 2 objects of Money
+            Console.WriteLine(euro > dollar);
+            Console.WriteLine(euro < dollar);
+            // compare 2nd object of Money and string
+            Console.WriteLine(euro > "13");
+            Console.WriteLine(euro < "13");
+            Console.WriteLine(euro > "1g3");
+            Console.WriteLine(euro < "13f");
+            // check CurrencyType of every object
+            if (dollar)
+            {
+                Console.WriteLine(dollar);
+            }
+            if (euro)
+            {
+                Console.WriteLine(euro);
+            }
+            dollar.CurrencyType = CurrencyTypes.UAH;
+            if (dollar)
+            {
+                Console.WriteLine("Working");
+            }
+            else
+            {
+                Console.WriteLine("No working");
+            }
+            // convert 1st object of Money to string
+            string dolString = dollar.ToString();
+            Console.WriteLine(dolString);
+
+            double money = (double)euro;
+            Console.WriteLine(money);
+
+            string moneyString = (string)euro;
+            Console.WriteLine(moneyString);
+
+            euro = (Money)89.93;
+            Console.WriteLine(euro);
+
+            euro = (Money)"99,93";
+            Console.WriteLine(euro);
+
+            euro = (Money)"89.93";
+            Console.WriteLine(euro);
+            Console.ReadLine();
+
+        }
+    }
+}
