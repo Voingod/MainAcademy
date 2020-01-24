3b1c61159d6b27fc50818d4be0f08f6eee883e3d
Voingod <voingodin@gmail.com>
2020-01-25 01:17:16 +0200

    #TDP-3.1. Рефакторинг кода
    Исправлено:
    1. Все, что выводиться в консоль, должно быть универсальным
    2. В методе SearchInformation повторяется логика, нужно поправить (сделать чтобьы метод можно было параметром передать)
    3. Вынес AirportPanel в отдельный файл класса
    4. Добавил публичный метод Menu, который вызывается из класса Program
    5. Методы для работы с классом AirportPanel сделал приватными, поскольку они вызываются через метод Menu
    6. ВЫнес перечисления в отдельный файл
    
    Известыне проблемы:
    1. Если список рейсов пустой, то не блочатся пункы с удалением, выводом чрезвычайной ситуации,
    изменением
    2. Нет описаний чрезвычайных ситуаций и статусов полета
    3. В перечислении чрезвычайных ситуаций нет пункта о его отсутсвии (пункт "все хорошо")
    4. Сортировка пузырькем
    5. Функции которые начинаются на Get должны что-то возвращать
    6. flight.Airline = AirportPanel.SetInfo() - Почему Airline  равно Info?  Выглядит как проблематичное место.
    7. Делать обертки над Console.WriteLine(), Console.Readline(), не нужно (имеется ввиду те функции, где вызывается только это)

---
+++ Project Airport panel (1.1 - 1.5)/Airport/Airport/EnumOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace Airport
+{
+    enum FlightStatus
+    {
+        checkIn, gateClosed, arrived, departedAt, unknown, canceled, expectedAt, delayed, inFlight
+    }
+    enum EmergencyInformation
+    {
+        evacuation, fire
+    }
+    enum ParamForSearch
+    {
+        flightNumber = 1, dataTimeOfarrival, arrivalPort, departurePort, nearestFlightArrived, nearestFlightDeparture
+    }
+    enum ParamMenu
+    {
+        createNewFlight = 1, deleteFlight, updateFlightsInformation, searchInformation, outputSchedule, outputEmergencyInformation
+    }
+    enum ParamUpdate
+    {
+        updateNumberOfGate = 1, updateFlightNumber, updateAirline, updateTerminal, updateFlightStatus, updateEmergencyInformation,
+        updateDateOfArrive, updateArrivalCity, updateArrivalPort, updateDateOfDeparture, updateDepartureCity, updateDeparturePort
+    }
+}
