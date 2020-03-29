70ce4b50aad15ee1e852369464d503cda3bf2952
Voingod <voingodin@gmail.com>
2020-03-29 23:01:54 +0300

    #WPF-2. Реализация доп.задания + разбор

---
+++ Module2/Lab 4.3/Hello_WPF_stud/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_WPF_stud
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public double Rate { get; set; }
+        public static ObservableCollection<Student> GetStudents()
+        {
+            ObservableCollection<Student> result = new ObservableCollection<Student>
+            {
+                new Student() { Name = "Andry Houp", Rate = 70 },
+                new Student() { Name = "Mary First", Rate = 64 },
+                new Student() { Name = "John Miller", Rate = 30 },
+                new Student() { Name = "Helen Best", Rate = 0 },
+                new Student() { Name = "Ivan Stown", Rate = 29 },
+                new Student() { Name = "Mishel Capoa", Rate = 91 }
+            };
+            return result;
+        }
+    }
+}
---
+++ Module2/Lab 4.3/Hello_WPF_stud/Window2.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Hello_WPF_stud
+{
+    /// <summary>
+    /// Логика взаимодействия для Students.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+
+        public static readonly DependencyProperty StudentsProperty =
+            DependencyProperty.Register("Students", typeof(ObservableCollection<Student>),
+                typeof(Window2), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DeleteCommandProperty =
+             DependencyProperty.Register("DeleteCommand", typeof(ICommand),
+                typeof(Window2), new PropertyMetadata(null));
+
+        public ObservableCollection<Student> Students
+        {
+            get { return (ObservableCollection<Student>)GetValue(StudentsProperty); }
+            set { SetValue(StudentsProperty, value); }
+        }
+
+        public ICommand DeleteCommand
+        {
+            get { return (ICommand)GetValue(DeleteCommandProperty); }
+            set { SetValue(DeleteCommandProperty, value); }
+        }
+        public Window2()
+        {
+            InitializeComponent();
+            Students = Student.GetStudents();
+            DeleteCommand = new MyCommand() { Collection = Students };
+        }
+
+        private void StudentBG_SelectedCellsChanged_1(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if (e.AddedCells.Count == 0) return;
+            var currentCell = e.AddedCells[0];
+            if (currentCell.Column == StudentBG.Columns[1])
+            {
+                StudentBG.BeginEdit();
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void List_Click(object sender, RoutedEventArgs e)
+        {
+            string mylist = null;
+
+            foreach (var item in Students)
+            {
+                mylist += item.Name.ToString() + " " + item.Rate.ToString();
+                mylist += "\r\n";
+            }
+
+            MessageBox.Show(mylist);
+        }
+    }
+    public class MyCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public ObservableCollection<Student> Collection { get; set; }
+
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => Collection.Remove(parameter as Student);
+    }
+}
