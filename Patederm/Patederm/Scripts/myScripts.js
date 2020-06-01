276897e8af9768bb8f6b3e535028f1d05c5bfc91
Voingod-HP <voingodin@gmail.com>
2020-06-01 22:42:40 +0300

    #TDP-8. - Правки 1. Вынес все стили CSS в отдельный файл 2. Поправил кнопку Save. Теперь она скрыта, пока не выведен результат кластеризации 3. Вынес скрипты в отдельынй файл

---
+++ Patederm/Patederm/Scripts/myScripts.js
@@ -0,0 +1,20 @@
+﻿function OnSuccess() {
+    if ($("#viewResults").length) {
+        $("#saveLink").show()
+    }
+    else {
+        $("#saveLink").hide()
+    }
+}
+
+$(function () {
+    $.ajaxSetup({ cache: false });
+    $(".saveData").click(function (e) {
+
+        e.preventDefault();
+        $.get(this.href, function (data) {
+            $('#dialogContent').html(data);
+            $('#modDialog').modal('show');
+        });
+    });
+})
